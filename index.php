4a3353b54813f600b5a6de2b282324e19302fbe0
Trent Sebastian Schotter <trentsebastian.schotter@tptlive.ee>
2026-09-02 13:21:01 +0300

    Add variable example

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo "Hello, Elliot";
+echo "Hello, Elliot"; // echoes
+
+$name = "Bintoki";
+$age = 25;
+echo $age;
+$age = $age * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
+echo $age;
+
+echo 2135125312635126325+219371287361263127863;
 ?>
