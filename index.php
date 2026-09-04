247264aa4ddccb97aa17d9efead816de81689c99
Trent Sebastian Schotter <trentsebastian.schotter@tptlive.ee>
2026-09-04 13:17:09 +0300

    john is 30

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php
-$test = 10;
-$test = 10 + 2;
-$test = 10 - 2;
-$test = 10 * 2;
-$test = 10 / 2;
-$test = $test - 1; //samad vaartused
-$test -= 1; //samad vaartused
-$test += 1; //samad vaartused
-$test *= 2; //samad vaartused
-$test /= 2; //samad vaartused
-$test = 10 % 3; //samad vaartused
-$test++; //tahendab +1
-$test--; // tahendab -1
-$test = (10 + (2 - 3) * 4) / 2; //sulud on olulised, sest need muudavad arvutuste järjekorda
-$test %= 2;
-$test = 2 ** 4; //2 astmel 4
-$test = sqrt(16); //ruutjuur
+$test = 'hello';
+$test = 'hello' . 'world';
+$test .= '!'; 
+$name = 'John';
+$age = '30';
+$test = $name . ' is ' . $age . ' years old.';
+$test = "$name is $age years old.";
+var_dump($test);
+$test = "hello\n\nworld";
 var_dump($test);
 ?>
