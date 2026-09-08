da4712f079a514987380ab48273900d6e594123f
Trent Sebastian Schotter <trentsebastian.schotter@tptlive.ee>
2026-09-08 11:12:09 +0300

    case

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
 <?php
-$test = [1, 2, 'word', true];
-var_dump($test[2]);
-$test[2] = 'new word';
-var_dump($test);
-$test[4] = 'yuppity';
-var_dump($test);
-$test[] = 'yup';
-var_dump($test);
-array_push($test, 'pushed value');
-var_dump($test);
-$test = [
-    'name' => 'tret',
-    'age' => 25,
-];
+$num = 10;
+if($num > 5) {
+    var_dump('Bigger');
+}
+
+if($num > 5) 
+    var_dump('Bigger');
+} else {
+    var_dump('Smaller');
+}
+
+$i = 1;
+
+switch($i) {
+    case 1:
+        var_dump('yx');
+        break;
+    case 2:
+        var_dump('kaks');
+        break;
+        case 3:
+        case 4:
+            var_dump('kolm neli');
+            break;
+        case 5:
+            var_dump('viis');
+            break;
+        case 6:
+            var_dump('kuus');
+            break;
+    default:
+        var_dump('dunno');
+}
