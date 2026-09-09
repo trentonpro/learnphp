1f3e4b141c9982a22d1ca032259269214a64e79e
trentschotter-sys <trent.schotter@gmail.com>
2026-09-09 13:03:30 +0300

    metalbox example

--- index.php
+++ index.php
@@ -21,13 +21,10 @@ class Box {
     }
 }
 
-$num = 1;
-$num =  &$num1; //pointer pass by reference
-$num1 = 2;
-var_dump($num1, $num2);
+class MetalBox extends Box { //inherits from box with extend
+    public $weight;
 
-$box1 = new Box();
-$box1->width = 1;
-$box2 = clone $box1; // use clone to create a copy of the object
-$box2->width = 2;
-var_dump($box1, $box2);
+    public function mass(){
+        return $this->volume() * $this->weight;
+    }
+}
