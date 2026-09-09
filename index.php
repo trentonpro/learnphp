2e3d6460041cfcd9ad1f7d8485940eefbe47803a
Trent Sebastian Schotter <trentsebastian.schotter@tptlive.ee>
2026-09-09 09:33:06 +0300

    function

--- index.php
+++ index.php
@@ -1,38 +1,41 @@
 <?php
 
-for($i=0; $i<10; $i++){
-
+function hello() {
+    var_dump('Hello');
 }
 
-for($i=9; $i>=0; $i--){
-    var_dump($i);
+hello();
+hello();
+hello();
+
+function helloNameandage ($name, $age, $debt) {
+    var_dump("Hello, $name. You are $age years old. You owe child support of $debt dollars.");
 }
 
-for($i=2; $i<=1_000_000; $i*=2){
-    var_dump($i);
+helloNameandage('Dale', 41, 115000);
+
+$test = function (){
+
+};
+
+var_dump($test);
+
+$numbers = [1,2,3,4,5];
+$squares = array_map(function ($n) {
+    return $n * $n;
+}, $numbers);
+$squares = array_map(fn ($n) => $n * $n, $numbers);
+var_dump($squares);
+
+function cube($a) {
+    if($a < 0) {
+        return 'Negative.';
+    }
+    return $a * $a * $a;
 }
 
-$time = time();
-$n = 0;
-while($time+1 > time()) {
-    $n++;
-}
-var_dump($n);
+var_dump(cube(4));
 
-while (false){
-    var_dump('while');
-}
-
-do {
-    var_dump('DO');
-} while (false);
-
-// while (true) {
-
-// }
-
-$fruits = ['apple', 'cherry', 'banana'];
-
-foreach ($fruits as $fruit) {
-    var_dump($fruit);
-}
+$answer = cube(5);
+$text = "cube of 5 is $answer";
+echo $text;
