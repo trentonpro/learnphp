19b420cf38436d6bcbf92c2f9cfbe08a5317c06f
Trent Sebastian Schotter <trentsebastian.schotter@tptlive.ee>
2026-09-15 11:24:49 +0300

    simple router script

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -7,9 +7,7 @@
           <div class="col-4 text-center">
             <a
               class="blog-header-logo text-body-emphasis text-decoration-none"
-              href="#"
-              >Large</a
-            >
+              href="#">Large</a>
           </div>
           <div class="col-4 d-flex justify-content-end align-items-center">
             <a class="link-secondary" href="#" aria-label="Search">
@@ -24,8 +22,7 @@
                 stroke-width="2"
                 class="mx-3"
                 role="img"
-                viewBox="0 0 24 24"
-              >
+                viewBox="0 0 24 24">
                 <title>Search</title>
                 <circle cx="10.5" cy="10.5" r="7.5"></circle>
                 <path d="M21 21l-5.2-5.2"></path>
@@ -37,13 +34,9 @@
       </header>
       <div class="nav-scroller py-1 mb-3 border-bottom">
         <nav class="nav nav-underline justify-content-between">
-          <a class="nav-item nav-link link-body-emphasis active" href="#"
-            >World</a
-          >
-          <a class="nav-item nav-link link-body-emphasis" href="#">U.S.</a>
-          <a class="nav-item nav-link link-body-emphasis" href="#"
-            >Technology</a
-          >
+          <a class="nav-item nav-link link-body-emphasis active" href="/">World</a>
+          <a class="nav-item nav-link link-body-emphasis" href="/us">U.S.</a>
+          <a class="nav-item nav-link link-body-emphasis" href="#">Technology</a>
           <a class="nav-item nav-link link-body-emphasis" href="#">Design</a>
           <a class="nav-item nav-link link-body-emphasis" href="#">Culture</a>
           <a class="nav-item nav-link link-body-emphasis" href="#">Business</a>
