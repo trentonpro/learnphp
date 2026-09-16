5b6ac3f3d9166b8d9f52a7386fac53c8817a1440
Trent Sebastian Schotter <trentsebastian.schotter@tptlive.ee>
2026-09-16 10:00:05 +0300

    SERVER

--- public/index.php
+++ public/index.php
@@ -1,48 +1,54 @@
 <?php
-if (preg_match('/\.(?:png|jpg|jpeg|gif|css)$/', $_SERVER["REQUEST_URI"])) {
-    return false; // serve the requested resource as-is.
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
 }
-
 function dump(...$vars)
 {
     echo '<pre>';
     var_dump(...$vars);
     echo '</pre>';
 }
-
-
-
-switch ($_SERVER['REQUEST_URI']) {
-    case '/':
-        $posts = [
-            [
-                'title' => 'Some world title 1',
-                'content' => 'Some world content 1',
-                'date' => 'January 1, 2021',
-                'author' => 'Pets',
-            ],
-            [
-                'title' => 'Some world title 2',
-                'content' => 'Some world content 2',
-                'date' => 'January 2, 2021',
-                'author' => 'Juss',
-            ],
-            [
-                'title' => 'Some world title 3',
-                'content' => 'Some world content 3',
-                'date' => 'January 3, 2021',
-                'author' => 'Alex',
-            ],
-            [
-                'title' => 'Some world title 4',
-                'content' => 'Some world content 4',
-                'date' => 'January 4, 2021',
-                'author' => 'Manivald',
-            ],
-        ];
-        include __DIR__ . '/../views/index.php';
-        break;
-    case '/us':
+ 
+spl_autoload_register(function ($class){
+    $class = substr($class,4);
+    $class = str_replace('\\', '/', $class);
+    require_once __DIR__ . '/../src/' . $class . '.php';
+});
+ 
+use App\Router;
+ 
+Router::addRoute('/', function () {
+    $title = 'World';
+    $posts = [
+        [
+            'title' => 'Some world title 1',
+            'content' => 'Some world content 1',
+            'date' => 'January 1, 2021',
+            'author' => 'Pets',
+        ],
+        [
+            'title' => 'Some world title 2',
+            'content' => 'Some world content 2',
+            'date' => 'January 2, 2021',
+            'author' => 'Juss',
+        ],
+        [
+            'title' => 'Some world title 3',
+            'content' => 'Some world content 3',
+            'date' => 'January 3, 2021',
+            'author' => 'Alex',
+        ],
+        [
+            'title' => 'Some world title 4',
+            'content' => 'Some world content 4',
+            'date' => 'January 4, 2021',
+            'author' => 'Manivald',
+        ],
+    ];
+    include __DIR__ . '/../views/index.php';
+});
+ 
+Router::addRoute('/us', function () {
         $title = 'U.S';
         $posts = [
             [
@@ -71,8 +77,25 @@ switch ($_SERVER['REQUEST_URI']) {
             ],
         ];
         include __DIR__ . '/../views/us.php';
-        break;
-    default:
-        echo '404';
-        break;
+    // include __DIR__ . '/../views/index.php';
+});
+
+$router = new Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if ($match) {
+    call_user_func($match['action']);
+} else {
+    echo 404;
 }
+
+// dump(Router::getRoutes());
+ 
+// switch ($_SERVER['REQUEST_URI']) {
+//     case '/':
+//         break;
+//     case '/us':
+//         break;
+//     default:
+//         echo '404';
+// }
+ 
